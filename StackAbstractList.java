09fe25b530cde8666e9121d4ad069a5646a711a1
HiramRV <rub15044@uvg.edu.gt>
2016-08-14 13:27:43 -0600

    Uso de List

--- StackAbstractList.java
+++ StackAbstractList.java
@@ -1,9 +1,28 @@
-package hdt4;
 
 public class StackAbstractList extends Factory{
 
+	private List Listas;
+	
+	public StackAbstractList(){
+		this.Listas = new List();
+	}
 	public void mensaje(){
 		System.out.println(" Lista ");
 		}
 
+
+
+
+	public List getListas() {
+		return Listas;
+	}
+
+
+
+
+	public void setListas(List listas) {
+		Listas = listas;
+	}
+
 }
+
