81bdabc70e7a557cff25b35cfd7bff2b433383d3
HiramRV <rub15044@uvg.edu.gt>
2016-08-15 17:52:55 -0600

    Encabezado

--- I_Calculadora.java
+++ I_Calculadora.java
@@ -1,8 +1,8 @@
 /**
 *I_Calculadora, Esta define los metodos que seran implementados en la calculadora
 *@version: 3.0
-*@author: Robbin Woods 15201, Steven Rubio 15044, Erick Bautista 15192
-*@since 2016-07-24
+*@author: Steven Rubio, 15044 // Andrea Pena 15127
+*@since 2016-08-15
 */
 
 
