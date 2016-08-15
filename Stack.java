1a7ac573c7e5cc0c7419cb238d83148bf540e82f
HiramRV <rub15044@uvg.edu.gt>
2016-08-15 12:46:07 -0600

    Encabezado

--- Stack.java
+++ Stack.java
@@ -1,7 +1,7 @@
 /**
 *Stack, Esta se comporta como un repositorio para almacenar los datos ingresados a la calculadora
 *@version: 3.0
-*@author: Robbin Woods 15201, Steven Rubio 15044, Erick Bautista 15192
+*@author: Andrea Pena 15127, Steven Rubio 15044
 *@since 2016-07-24
 */
 
